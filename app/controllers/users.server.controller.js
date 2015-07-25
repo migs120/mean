1ad23f8b88f1c7207066842436ce594376cc3d45
Your Name <you@example.com>
2015-07-25 11:45:32 +0000

    bower-works

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -114,13 +114,14 @@ exports.list = function(req, res, next) {
 
 exports.update = function(req, res, next) {
                                              User.findByIdAndUpdate(req.user.id, req.body, function(err, user) {
-                                                        if (err) {
-                                                                  return next(err);
-                                                                 } 
-                                                       else {
-                                                              res.json(user);
-                                                            }
-                                                  });
+                                                                                                                 if (err) {
+                                                                                                                            return next(err);
+                                                                                                                           } 
+                                                                                                                  else {
+                                                                                                                          res.json(user);
+                                                                                                                        }
+                                                                                                              }
+                                                                   );
                                              };
 
 exports.delete = function(req, res, next) {
@@ -133,6 +134,49 @@ exports.delete = function(req, res, next) {
                                                                                 }
                                                                           })
                                           };
+
+
+//======oAuth for facebook, twitter and google login ===========================
+
+exports.saveOAuthUserProfile = function(req, profile, done) {
+                                                              User.findOne({
+                                                                              provider: profile.provider,
+                                                                              providerId: profile.providerId
+                                                                            }, 
+                                                                           function(err, user) {
+                                                                                                if (err) {
+                                                                                                            return done(err);
+                                                                                                          } 
+                                                                                                 else {
+                                                                                                                if (!user) {
+                                                                                                                              var possibleUsername = profile.username || ((profile.email) ? profile.email.split('@')[0] : '');                   
+
+                                                                                                                              User.findUniqueUsername(possibleUsername, null, function(availableUsername) {
+                                                                                                                                                                                                            profile.username = availableUsername;
+
+                                                                                                                                                                                                            user = new User(profile);
+
+                                                                                                                                                                                                            user.save(function(err) {
+                                                                                                                                                                                                                                        if (err) {
+                                                                                                                                                                                                                                                  var message = _this.getErrorMessage(err);                                             
+
+                                                                                                                                                                                                                                                   req.flash('error', message);
+                                                                                                                                                                                                                                                    return res.redirect('/signup');
+                                                                                                                                                                                                                                                  }
+
+                                                                                                                                                                                                                                        return done(err, user);
+                                                                                                                                                                                                                                      }
+                                                                                                                                                                                                                      );
+                                                                                                                                                                                                          }
+                                                                                                                                                     );
+                                                                                                                            } 
+                                                                                                                 else {
+                                                                                                                        return done(err, user);
+                                                                                                                      }
+                                                                                                      }
+                                                                                              });
+                                                            };
+
 
 //===============this following section must remain at the bottom or else create update delete wont work!!!!=========
 
